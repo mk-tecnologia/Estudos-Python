3cb80be815b088a9fc340ab7095f74e42088a702
Marcos A <marcos@mktecnologia.net.br>
2019-01-21 19:25:57 -0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/misc.xml

---
+++ Mundo 01/ex009 Tabuada.py
@@ -0,0 +1,18 @@
+'''
+Simples programa que lê um número e mostra a sua tabuada
+'''
+print('##### FAÇA UM PGM QUE LEIA UM NUMERO E FAÇA A SUA TABUADA #####')
+num = int(input('Digite um numero para ver a sua tabuada\n'))
+print(f'A tabuada de {num} é a seguinte:')
+print('='*17)
+print(f'|| {num} * {1:2} = {num * 1:2} ||')
+print(f'|| {num} * {2:2} = {num * 2:2} ||')
+print(f'|| {num} * {3:2} = {num * 3:2} ||')
+print(f'|| {num} * {4:2} = {num * 4:2} ||')
+print(f'|| {num} * {5:2} = {num * 5:2} ||')
+print(f'|| {num} * {6:2} = {num * 6:2} ||')
+print(f'|| {num} * {7:2} = {num * 7:2} ||')
+print(f'|| {num} * {8:2} = {num * 8:2} ||')
+print(f'|| {num} * {9:2} = {num * 9:2} ||')
+print(f'|| {num} * {10:2} = {num * 10:2} ||')
+print('='*17)
