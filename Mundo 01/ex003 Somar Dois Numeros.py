71777811386489ed385ac984c743fab21f94a582
Marcos A <marcos@mktecnologia.net.br>
2019-01-21 19:01:27 -0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/misc.xml

--- Mundo 01/ex003 Somar Dois Numeros.py
+++ Mundo 01/ex003 Somar Dois Numeros.py
@@ -4,4 +4,4 @@ Simples Programa que pergunta dois valores e os soma
 print('##### FAÇA UMA PGM QUE LEIA DOIS NUMEROS E SOME ELES #####')
 num1 = int(input('Informe o primeiro valor\n'))
 num2 = int(input('Informe o segundo valor\n'))
-print(f'{num1} + {num2} = {num1+num2}')
+print(f'{num1} + {num2} = {num1 + num2}')
--- Mundo 01/ex005 Antecessor e Sucessor.py
+++ Mundo 01/ex005 Antecessor e Sucessor.py
@@ -3,4 +3,4 @@ Simples Programa que lê um numero e escreve seu sucessor e seu antecessor
 '''
 print('##### FAÇA UM PGM QUE LEIA UM NUMERO E ESCREVA SEU ANTECESSOR E SUCESSOR #####')
 n = int(input('Digite um numero:\n'))
-print(f'Você digitou {n} seu antecessor é {n-1} e seu sucessor é {n+1}')
+print(f'Você digitou {n} seu antecessor é {n - 1} e seu sucessor é {n + 1}')
