5f3d112bf427bbeac71e1064c6731ee568a1f9f4
Marcos A <marcos@mktecnologia.net.br>
2019-01-21 19:04:55 -0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/misc.xml

---
+++ Mundo 01/ex007 Média Aritimética.py
@@ -0,0 +1,7 @@
+'''
+Simples Programa que lê duas notas e calcula a média aritimética
+'''
+print('##### FAÇA UM PGM QUE LEIA DUAS NOTAS E CALCULE A MEDIA ARITIMETICA #####')
+n1 = float(input('Qual foi a nota1?\n'))
+n2 = float(input('Qual foi a nota2?\n'))
+print(f'A média entre a nota {n1} e a mota {n2} é {((n1 + n2) / 2):.1f}')
