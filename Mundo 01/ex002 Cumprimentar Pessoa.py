9c54c7f22d7057f8346e018194d61d8149e2b3dc
Marcos A <marcos@mktecnologia.net.br>
2019-01-21 18:28:49 -0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/misc.xml

---
+++ Mundo 01/ex002 Cumprimentar Pessoa.py
@@ -0,0 +1,6 @@
+'''
+Simples Programa que pergunta o nome e escreve uma mensagem de boas-vindas na tela
+'''
+print('##### FAÇA UM PGM QUE ESCREVA O NOME DA PESSOA E CUMPRIMENTE ELA #####')
+nome = input('Qual o seu nome?\n')
+print(f'Olá {nome} Seja bem-vindo!')
