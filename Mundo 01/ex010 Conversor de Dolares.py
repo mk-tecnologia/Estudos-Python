4af612653f19c4343c8e4b7ab12137a4cdb4f4ba
Marcos A <marcos@mktecnologia.net.br>
2019-01-21 19:29:08 -0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/misc.xml

---
+++ Mundo 01/ex010 Conversor de Dolares.py
@@ -0,0 +1,6 @@
+'''
+Simples programa para conversão de dolares
+'''
+print('##### FAÇA UM PGM QUE TRANSFORME REAIS EM DOLARES (1 REAL = 3,27 DOLARES)#####')
+num = float(input('Quantos reais vc quer converter?\n'))
+print(f'R${num} é equivalente a ${(num / 3.27):.2f}')
