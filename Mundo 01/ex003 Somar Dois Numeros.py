095e63f3fc17004ad7ec4c8d535e2cc8caed4a04
Marcos A <marcos@mktecnologia.net.br>
2019-01-21 18:33:19 -0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/misc.xml

---
+++ Mundo 01/ex003 Somar Dois Numeros.py
@@ -0,0 +1,7 @@
+'''
+Simples Programa que pergunta dois valores e os soma
+'''
+print('##### FAÇA UMA PGM QUE LEIA DOIS NUMEROS E SOME ELES #####')
+num1 = int(input('Informe o primeiro valor\n'))
+num2 = int(input('Informe o segundo valor\n'))
+print(f'{num1} + {num2} = {num1+num2}')
