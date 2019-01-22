523a2dd885b46e0277f6013cd8e503426ff64c14
Marcos A <marcos@mktecnologia.net.br>
2019-01-22 07:49:15 -0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/misc.xml

--- Mundo 01/ex017 Calculo da Hiponetusa.py
+++ Mundo 01/ex017 Calculo da Hiponetusa.py
@@ -1,10 +1,10 @@
 '''
-Simples programa que calcula a hipotenuza de um triângulo retangulo
+Simples programa que calcula a hipotenusa de um triângulo retangulo
 '''
 from math import hypot
 print('##### CRIE UM PGM QUE CALCULE A HIPOTENUSA DE UM TRIANGULO RETANGULO #####')
 co = float(input('Digite o valor do cateto oposto\n'))
 ca = float(input('Digite o valor do cateto adjacente\n'))
-print(f'Em um triangulo retangulo com cateto oposto de valor {co} e cateto adjacente {ca} o valor da hipotenuza é:'
+print(f'Em um triangulo retangulo com cateto oposto de valor {co} e cateto adjacente {ca} o valor da hipotenusa é:'
       f' {hypot(ca, co):.2f}')
 
