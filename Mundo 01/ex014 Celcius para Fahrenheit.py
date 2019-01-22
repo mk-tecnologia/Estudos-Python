48b6e3a547c416ac70bffdca1bab8411548c1995
Marcos A <marcos@mktecnologia.net.br>
2019-01-22 07:31:00 -0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/misc.xml

---
+++ Mundo 01/ex014 Celcius para Fahrenheit.py
@@ -0,0 +1,6 @@
+'''
+Simples programa para conversão de graus celcius em fahrenheit
+'''
+print('##### FAÇA UM PGM QUE CONVERTA GRAUS CELCIUS EM FAHRENHEIT #####')
+c = float(input('Informe e temperatura em C0: '))
+print(f'{c}C0 equivalem a {((1.8 * c) + 32):.2f}F0')
